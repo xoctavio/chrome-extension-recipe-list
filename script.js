0220396e0c36494fe618b389a815cb05e621982e
xoctavio <xavieroctavio91@hotmail.com>
2022-12-13 11:09:17 -0600

    Add event listeners

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ if (listFromLocalStorage) {
  recipes = listFromLocalStorage;
  show(recipes);
 }
+
+// event listeners
+saveBtn.addEventListener("click", function() {
+ recipes.push(inputEl.value);
+ inputEl.value = "";
+ localStorage.setItem("recipes", JSON.stringify(recipes));
+ show(recipes);
+ console.log(recipes);
+});
+
+pressEnter.addEventListener("keypress", function(event) {
+ if (event.key === "Enter") {
+  // event.preventDefault();
+  document.querySelector("#save-btn").click();
+ }
+}); 
+
+tabBtn.addEventListener("click", function() {
+ chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  recipes.push(tabs[0].url);
+  localStorage.setItem("recipes", JSON.stringify(recipes));
+  show(recipes) });
+});
+
+deleteBtn.addEventListener("click", function() {
+ localStorage.clear();
+ recipes = [];
+ show(recipes);
+});
